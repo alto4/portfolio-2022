890e4d4e2ad8045a194d99a59eeb8449ceabf707
Scott Alton <new@MacBook-Pro.local>
2022-12-31 13:19:53 -0500

    Revert test commit.

--- src/components/sections/Hero.jsx
+++ src/components/sections/Hero.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 const Hero = () => {
   return (
     <section className='row hero'>
-      <h1 className='hero-title'>Scott W. Alton</h1>
+      <h1 className='hero-title'>Scott Alton</h1>
       <p className='hero-subtext'>
         I'm a full-stack developer from the Greater Toronto Area specializing in interactive, data-driven UIs that
         provide an engaging user experience.
